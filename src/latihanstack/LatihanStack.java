33a724eed3e5471ca6a21fbf2bde7fb9d2eba700
Ibnu Halim Mustofa <ibnuhalimm@gmail.com>
2019-06-23 22:26:55 +0700

    first commit

---
+++ src/latihanstack/LatihanStack.java
@@ -0,0 +1,41 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package latihanstack;
+
+/**
+ *
+ * @author dev
+ */
+
+import java.util.Stack;
+
+public class LatihanStack {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Stack<Character> s1 = new Stack<>();
+        Stack<Character> s2 = new Stack<>();
+        Stack<Character> s3 = new Stack<>();
+        
+        s1.push('D');
+        s1.push('A');
+        s1.push('T');
+        s1.push('A');
+        
+        while (!s1.empty()) {
+            s2.push(s1.pop());
+        }
+        
+        while (!s2.empty()) {
+            System.out.print(s2.pop());
+        }
+        
+        System.out.println();
+    }
+    
+}
